ec6d2c8c483ebd6387a2a3a181783c212df033e4
Lossydragon <thedragon1012@gmail.com>
2023-01-22 04:31:34 -0600

    Use splashscreen

--- PreferencesManager/src/main/java/fr/simon/marquis/preferencesmanager/LaunchActivity.kt
+++ PreferencesManager/src/main/java/fr/simon/marquis/preferencesmanager/LaunchActivity.kt
@@ -1,22 +1,27 @@
 package fr.simon.marquis.preferencesmanager
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.topjohnwu.superuser.Shell
 import fr.simon.marquis.preferencesmanager.ui.applist.AppListActivity
+import kotlinx.coroutines.launch
 
 /**
  * A Splash activity to launch libsu shell request
  */
+@SuppressLint("CustomSplashScreen")
 class LaunchActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // libsu shell
-        Shell.getShell {
-            val intent = Intent(this, AppListActivity::class.java)
-            startActivity(intent)
+        installSplashScreen().setKeepOnScreenCondition { true }
+        lifecycleScope.launch {
+            Shell.getShell()
+            startActivity(Intent(this@LaunchActivity, AppListActivity::class.java))
             finish()
         }
     }
